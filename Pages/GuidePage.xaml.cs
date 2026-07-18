fd95e30e62accb5eb9be78743bd3999d9eb02f81
R1Xern <joycecurcirt539-dot@users.noreply.github.com>
2026-07-19 02:51:53 +0800

    feat: expand trusted developers links and projects in guide page

--- Pages/GuidePage.xaml.cs
+++ Pages/GuidePage.xaml.cs
@@ -17,6 +17,21 @@ public sealed partial class GuidePage : Page
         OpenUrl("https://github.com/bol-van/zapret");
     }
 
+    private void OpenFlowsealGithub_Click(object sender, RoutedEventArgs e)
+    {
+        OpenUrl("https://github.com/Flowseal");
+    }
+
+    private void OpenGoodbyeDpiGithub_Click(object sender, RoutedEventArgs e)
+    {
+        OpenUrl("https://github.com/ValdikSS/GoodbyeDPI");
+    }
+
+    private void OpenWinDivertGithub_Click(object sender, RoutedEventArgs e)
+    {
+        OpenUrl("https://github.com/basil00/Divert");
+    }
+
     private void OpenMirrlyGithub_Click(object sender, RoutedEventArgs e)
     {
         OpenUrl("https://github.com/joycecurcirt539-dot/Zapret-Mirrly-GUI");
